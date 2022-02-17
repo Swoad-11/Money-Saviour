807911b37e8b98aba9b0a4ac551444088be94038
Swoad-11 <toufiq11swoad@gmail.com>
2022-02-17 16:20:29 +0600

    calculation done

--- javascript.js
+++ javascript.js
@@ -1,17 +1,44 @@
-function getValue(value){
-    const givenInputs = document.getElementById(value);
+function getValue(inputAmount){
+    const givenInputs = document.getElementById(inputAmount);
     const givenAmounts = givenInputs.value;
      return givenAmounts;
 }
 
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    const incomeInput = getValue(inputIncome);
-    const expFood = getValue(foodExpense);
-    const expRent = getValue(rentExpense);
-    const expCloth = getValue(clothExpense);
+// Calculation
+function calculation(clickInput){
+    const incomeInput = getValue('inputIncome');
+    const expFood = getValue('foodExpense');
+    const expRent = getValue('rentExpense');
+    const expCloth = getValue('clothExpense');
+    const savePerecentage = getValue('saveInput');
 
-    const totalExpense = expFood + expRent + expCloth;
-    totalExpenseDisplay.innerText = totalExpense;
+    const totalExpense = parseFloat(expFood) + parseFloat(expRent) + parseFloat(expCloth);
     const remainingBalance = incomeInput - totalExpense;
-    balanceDisplay.innerText = remainingBalance
+    const pSaving = (parseFloat(savePerecentage)/100)*incomeInput;
+    const finalBalance = remainingBalance - pSaving;
+
+
+    if( clickInput == true){    //onClick for Calculate button
+        totalExpenseDisplay.innerText = totalExpense;
+        balanceDisplay.innerText = remainingBalance;
+    }
+    else{      //onClick for save button
+        savingAmountDisplay.innerText = pSaving;
+        remainingBalanceDisplay.innerText = finalBalance;
+    }
+}
+
+
+// incomeExpense 
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const calculate = true;
+    calculation(calculate);
+});
+
+// saving 
+
+document.getElementById('save-btn').addEventListener('click', function(){
+    const save = false;
+    calculation(save);
 });
